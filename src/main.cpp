2658b2b6a4f23e004fb2ee438e122a222f3a2439
nicklasvsams <nicklasvsams@gmail.com>
2023-01-17 10:28:58 +0100

    The RTOS semaphore program.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,116 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>  // add the FreeRTOS functions for Semaphores.
 
+#define greenLED 13
+#define redLED 12
+#define tickTimeToWait 5
+#define delayTime 1000
+
+// Declare a binary Semaphore Handle which we will use to manage the Serial Port.
+// It will be used to ensure only one Task is accessing this resource at any time.
+SemaphoreHandle_t xSerialSemaphore;
+
+// define two Tasks for DigitalRead & AnalogRead
+void TaskGreenLEDBlink( void *pvParameters );
+void TaskRedLEDBlink( void *pvParameters );
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+
+  // Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
+  // because it is sharing a resource, such as the Serial port.
+  // Semaphores should only be used whilst the scheduler is running, but we can set it up here.
+  if ( xSerialSemaphore == NULL )  // Check to confirm that the Serial Semaphore has not already been created.
+  {
+    xSerialSemaphore = xSemaphoreCreateBinary();  
+    if ( ( xSerialSemaphore ) != NULL )
+      xSemaphoreGive( ( xSerialSemaphore ) );  // Make the Serial Port available for use, by "Giving" the Semaphore.
+  }
+
+  // Now set up two Tasks to run independently.
+  xTaskCreate(
+    TaskGreenLEDBlink
+    ,  "GreenLEDBlink"  // A name just for humans
+    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL //Parameters for the task
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL ); //Task Handle
+
+  xTaskCreate(
+    TaskRedLEDBlink
+    ,  "RedLEDBlink" // A name just for humans
+    ,  128  // Stack size
+    ,  NULL //Parameters for the task
+    ,  1  // Priority
+    ,  NULL ); //Task Handle
+
+  // Now the Task scheduler, which takes over control of scheduling individual Tasks, is automatically started.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Empty. Things are done in Tasks.
+}
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void TaskGreenLEDBlink( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
+  // make the green LED pin an input:
+  pinMode(greenLED, OUTPUT);
+
+  for (;;) // A Task shall never return or exit.
+  {
+    // See if we can obtain or "Take" the Serial Semaphore.
+    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
+    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) tickTimeToWait ) == pdTRUE )
+    {
+
+      // Make the LED blink for 1s, then wait 1s before "giving" semaphore away.
+      digitalWrite(greenLED, HIGH);
+      delay(delayTime);              
+      digitalWrite(greenLED, LOW);   
+      Serial.println("Green LED has blinked!");
+      delay(delayTime);    
+
+      xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
+    }
+
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
+
+void TaskRedLEDBlink( void *pvParameters __attribute__((unused)) )  // This is a Task.
+{
+  // make the red LED pin an input:
+  pinMode(redLED, OUTPUT);
+
+  for (;;) // A Task shall never return or exit.
+  {
+    // See if we can obtain or "Take" the Serial Semaphore.
+    // If the semaphore is not available, wait 5 ticks of the Scheduler to see if it becomes free.
+    if ( xSemaphoreTake( xSerialSemaphore, ( TickType_t ) tickTimeToWait ) == pdTRUE )
+    {
+      // Make the LED blink for 1s, then wait 1s before "giving" semaphore away.
+      digitalWrite(redLED, HIGH);
+      delay(delayTime);              
+      digitalWrite(redLED, LOW);   
+      Serial.println("Red LED has blinked!");
+      delay(delayTime);    
+
+      xSemaphoreGive( xSerialSemaphore ); // Now free or "Give" the Serial Port for others.
+    }
+
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }  
 }
